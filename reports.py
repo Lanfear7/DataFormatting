ddbff6f7c050be33881fdb5107f1b9574fe57d91
ghost <brennan1086@hotmai.com>
2020-10-09 20:55:29 -0700

    need to finish up the formatted text

--- reports.py
+++ reports.py
@@ -1,13 +1,41 @@
 from datetime import datetime
+import data
+
 
 def date():
     now = datetime.now()
     # dd/mm/YY H:M:S
     dt_string = now.strftime("%Y-%m-%d %H:%M")
-    print("date and time =", dt_string)
+    print("Timestamp", dt_string)
 
-def text_report():
-    pass
+
+def text_report(input_type):
+    date()
+    total = 0
+    print('Device: ' + input_type.capitalize())
+    if input_type == 'phone':
+        phone_data = data.get_phone_info_list()
+        phone_arr = []
+        for i in phone_data:
+            each = i
+            each_arr = each.split(',')
+            phone_arr.append(each_arr)
+        print('Number: ' + str(len(phone_arr)))
+        for j in phone_arr:
+
+            price = j[6]
+            total = total + float(price)
+            average = total / len(phone_arr)
+
+        text = f"{average:.2f}"
+        formatted = float(text)
+        print(f'Average Price: {formatted}')
+
+        print(f'Minimum price:')
+        print(f'Maximum price:')
+        print(f'Median RAM: ')
+        print(f'Operating system:')
+
 
 
 def csv_report():
@@ -16,3 +44,4 @@ def csv_report():
 
 def json_report():
     pass
+
