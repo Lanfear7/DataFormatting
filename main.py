57d93e2a6b6694ed4eab2b3ee61c02d08a394121
ghost <brennan1086@hotmai.com>
2020-10-22 02:23:49 -0700

    text formatting is almost done JSON aswell

---
+++ main.py
@@ -0,0 +1,63 @@
+import reports, data, sys
+
+input_type = sys.argv[1].lower()
+report_type = sys.argv[2].lower()
+
+def main(input_type, report_type):
+    if input_type == 'phone':
+        print('phone')
+        phone_data = data.get_phone_info_list()
+        phone_list = []
+        for i in phone_data:
+            i.split(',')
+            phone_list.append(i)
+        #pass in list into reports function so they can use them
+        if report_type == 'text':
+            print('text')
+            report = report_type
+            reports.text_report(phone_list, input_type)
+        elif report_type == 'csv':
+            print('csv')
+        elif report_type == 'json':
+            print('json')
+        else:
+            print('Report type must be either text, csv or json')
+    elif input_type == 'tablet':
+        print('tablet')
+        tablet_data = data.get_tablet_info_list()
+        tablet_list = []
+        for i in tablet_data:
+            i.split(',')
+            tablet_list.append(i)
+        print(tablet_list)
+        if report_type == 'text':
+            print('text')
+        elif report_type == 'csv':
+            print('csv')
+        elif report_type == 'json':
+            print('json')
+        else:
+            print('Report type must be either text, csv or json')
+    elif input_type == 'laptop':
+        print('laptop')
+        laptop_data = data.get_laptop_info_list()
+        laptop_list = []
+        for i in laptop_data:
+            i.split(',')
+            laptop_list.append(i)
+        print(laptop_list)
+        if report_type == 'text':
+            print('text')
+        elif report_type == 'csv':
+            print('csv')
+        elif report_type == 'json':
+            print('json')
+        else:
+            print('Report type must be either text, csv or json')
+    else:
+        print('Input type must be either phone, table or laptop ')
+
+
+
+if __name__=='__main__':
+    main(input_type, report_type)
--- reports.py
+++ reports.py
@@ -1,47 +1,76 @@
 from datetime import datetime
-import data
+#dont import data go data to main then to reports (data => main <=> reports)
+import main
 
+def text_report(list_t, input_type):
+    '''list_t = literal list  of items (phone, laptop, tablet) *** input_type = item to generate report for (phone, tablet, laptop) '''
+    print('*****in reports****')
 
-def date():
+    #Timestamp
     now = datetime.now()
     # dd/mm/YY H:M:S
     dt_string = now.strftime("%Y-%m-%d %H:%M")
-    print("Timestamp", dt_string)
+    print(f"Timestamp {dt_string}")
 
+    #Device
+    print(f'Device: {input_type.capitalize()}')
+    #Number
+    print(f'Number: {len(list_t)}')
+    #Average price
+    total = 0 
+    for i in list_t:
+        i_split = i.split(',')
+        total += float(i_split[6])
+    average = total / len(list_t)
+    print(f'Average price: ${average:.2f}')
+        
+    #Min price
 
-def text_report(input_type):
-    date()
-    total = 0
-    print('Device: ' + input_type.capitalize())
-    if input_type == 'phone':
-        phone_data = data.get_phone_info_list()
-        phone_arr = []
-        for i in phone_data:
-            each = i
-            each_arr = each.split(',')
-            phone_arr.append(each_arr)
-        print('Number: ' + str(len(phone_arr)))
-        for j in phone_arr:
+    #Max price
 
-            price = j[6]
-            total = total + float(price)
-            average = total / len(phone_arr)
-
-        text = f"{average:.2f}"
-        formatted = float(text)
-        print(f'Average Price: {formatted}')
-
-        print(f'Minimum price:')
-        print(f'Maximum price:')
-        print(f'Median RAM: ')
-        print(f'Operating system:')
-
-
+    #median ram
+    ram = 0 
+    for i in list_t:
+        i_split = i.split(',')
+        ram += int(i_split[3])
+    average_ram = ram / len(list_t)
+    if average_ram <= 20:
+        new_ram = 8
+        print(f'Median RAM: {new_ram}')
+    elif average_ram >20.0 and average_ram <48.0:
+        new_ram = 32
+        print(f'Median RAM: {new_ram} GB')
+    elif average_ram > 20 and average_ram > 48:
+        new_ram = 62
+        print(f'Median RAM: {new_ram}')
+    #os 
+    master = ''
+    for i in list_t:
+        i_split = i.split(',')
+        both = i_split[4:6]
+        a = both.pop(0)
+        b = both.pop(0)
+        all_string = f' {a} {b},'
+        master += all_string
+    last_comma = master.rstrip(',')
+    print(f'Operating System:{last_comma}')
 
 def csv_report():
     pass
 
 
 def json_report():
-    pass
+    now_date = date()
+    print(now_date)
+    print('{' '\n'
+        f'      \"date_time\": {now_date}''\n'
+        f'      \"device_type\": phone''\n'
+        f'      \"number\": phone''\n'
+        f'      \"average_price\": phone''\n'
+        f'      \"min_price\": phone''\n'
+        f'      \"max_price\": phone''\n'
+        f'      \"median_price\": phone''\n'
+        f'      \"operating_system\": phone''\n'
+        '}')
+
 
